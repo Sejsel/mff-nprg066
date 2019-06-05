07c02f921397b7b804b148e682d336f95358d501
Jiří Sejkora <sejsel@sejsel.cz>
2019-06-05 05:46:32 +0200

    Add the p, newline and H commands

--- src/ed.c
+++ src/ed.c
@@ -81,25 +81,120 @@ text *read_text(FILE *file) {
 	return text;
 }
 
+void print_error_message(char *message, bool verbose) {
+	puts("?");
+	if (verbose) {
+		puts(message);
+	}
+}
+
+bool ensure_no_suffix(char **error, int length, int pos, bool verbose) {
+	if (length > pos + 1) {
+		*error = "Unknown command suffix";
+		print_error_message(*error, verbose);
+		return false;
+	}
+
+	return true;
+}
+
+bool ensure_range_valid(char **error, size_t from, size_t to, text *text, bool verbose) {
+	if (from < 1 || to > text->lineCount || from > to) {
+		*error = "Invalid address";
+		print_error_message(*error, verbose);
+		return false;
+	}
+	return true;
+}
+
+bool ensure_no_range_set(char **error, bool rangeSet, bool verbose) {
+	if (rangeSet) {
+		*error = "Unexpected address";
+		print_error_message(*error, verbose);
+		return false;
+	}
+	return true;
+}
+
+void print_range(text *text, size_t from, size_t to) {
+	line *line = text->firstLine;
+	for (size_t i = 1; i <= to; i++) {
+		if (i >= from) {
+			fputs(line->text, stdout);
+		}
+		line = line->next;
+	}
+}
+
 int handle_input(text *text) {
-	size_t inputBufferSize = 100;
-	char *buffer = malloc(inputBufferSize * sizeof(*buffer));
+	char *lastError = NULL;
+	bool verbose = false;
+	int currentLine = text->lineCount;
 
-	int error = 0;
+	char *input;
 
-	while (fgets(buffer, inputBufferSize, stdin)) {
-		if (!strcmp(buffer, "q\n")) {
-			break;
+	while ((input = read_line(stdin)) != NULL) {
+		int length = strlen(input);
+		int pos;
+		int lineFrom;
+		int lineTo;
+		char command;
+		bool rangeSet = false;
+		if (sscanf(input, "%d,%d%c%n", &lineFrom, &lineTo, &command, &pos) == 3) {
+			// Address range
+			//printf("Read command, range: %d to %d, command: %c (pos: %d)\n", lineFrom, lineTo, command, pos);
+			rangeSet = true;
+		} else if (sscanf(input, "%d%c%n", &lineFrom, &command, &pos) == 2) {
+			// Address
+			//printf("Read command, line: %d, command: %c (pos: %d)\n", lineFrom, command, pos);
+			rangeSet = true;
+			lineTo = lineFrom;
+		} else if (sscanf(input, "%c%n", &command, &pos) == 1) {
+			// No address
+			//printf("Read command, command: %c (pos: %d)\n", command, pos);
+			lineFrom = currentLine;
+			lineTo = currentLine;
 		} else {
-			if (text == NULL) {
-				error = 1;
+			// Should never happen as there will always be at least a newline or NULL is returned
+		}
+
+		free(input);
+
+		if (command == 'H') {
+			if (!ensure_no_suffix(&lastError, length, pos, verbose)) continue;
+			if (rangeSet) {
+				if (!ensure_range_valid(&lastError, lineFrom, lineTo, text, verbose)) continue;
+				if (!ensure_no_range_set(&lastError, rangeSet, verbose)) continue;
 			}
 
-			puts("?");
+			verbose = !verbose;
+			if (verbose && lastError != NULL) {
+				puts(lastError);
+			}
+		} else if (command == 'p') {
+			if (!ensure_no_suffix(&lastError, length, pos, verbose)) continue;
+			if (!ensure_range_valid(&lastError, lineFrom, lineTo, text, verbose)) continue;
+
+			print_range(text, lineFrom, lineTo);
+			currentLine = lineTo;
+		} else if (command == '\n') {
+			if (!rangeSet) {
+				lineFrom++;
+				lineTo++;
+			}
+
+			if (!ensure_no_suffix(&lastError, length, pos, verbose)) continue;
+			if (!ensure_range_valid(&lastError, lineFrom, lineTo, text, verbose)) continue;
+
+			print_range(text, lineFrom, lineTo);
+			currentLine = lineTo;
+		} else {
+			lastError = "Unknown command";
+			print_error_message(lastError, verbose);
 		}
 	}
 
-	return error;
+	return lastError != NULL;
 }
 
 int main(int argc, char ** argv) {
@@ -124,6 +219,12 @@ int main(int argc, char ** argv) {
 	text *text = NULL;
 	if (file != NULL) {
 		text = read_text(file);
+	} else {
+		text = calloc(1, sizeof(*text));
+	}
+
+	if (text != NULL) {
+		printf("%zu\n", text->characterCount);
 	}
 
 	int error = handle_input(text);
